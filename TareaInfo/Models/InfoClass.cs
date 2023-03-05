48dfd5cf4a12a5e8ca8679e9f63354c5f33c015b
SAG <SAG@DIT>
2023-03-04 19:54:52 -0600

    Agregar archivos de proyecto.

---
+++ TareaInfo/Models/InfoClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TareaInfo.Models
+{
+    public class InfoClass
+    {
+        
+        public string nombre { get; set; }
+
+        public string apellido { get; set; }
+
+        public int edad { get; set; }
+
+        public string correo { get; set; }
+
+
+
+    }
+}
---
+++ TareaInfo/VIews/PageInfo.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TareaInfo.VIews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageInfo : ContentPage
+    {
+        public PageInfo()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnmostrar_Clicked(object sender, EventArgs e)
+        {
+            var info = new Models.InfoClass
+            {
+                nombre = txtnombre.Text,
+                apellido = txtapellido.Text,
+                edad = int.Parse(txtedad.Text),
+                correo = txtcorreo.Text,
+
+
+            };
+            
+
+            var page = new VIews.Resultados();
+            page.BindingContext= info;
+            await Navigation.PushAsync(page);
+
+            
+
+        }
+    }
+}
